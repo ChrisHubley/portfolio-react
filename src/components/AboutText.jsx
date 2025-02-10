c2adfa7e895364c24769b2e35c1ecd45d91adbd1
Chris Hubley <chrisjhubley@gmail.com>
2025-02-10 17:54:32 +0000

    finished about section

--- src/components/AboutText.jsx
+++ src/components/AboutText.jsx
@@ -5,7 +5,17 @@ export default function AboutText(){
     return(
         <div>
             <H2 text="About"/> 
-            <P text="text"/>
+            <div className="grid grid-cols-[1fr_3fr]">
+                <span></span>
+                <p className="
+                    text-sm 
+                    font-sans 
+                    p-20px
+                    bg-white
+                    mt-[20px] mr-[30px] mb-0 ml-0
+                    p-[10px]
+                ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt expedita quibusdam perspiciatis odio, nostrum voluptate similique reiciendis reprehenderit optio numquam ullam explicabo, porro, placeat quas distinctio iure mollitia magni incidunt.</p>
+            </div>
         </div>
     )
 }
--- src/components/Atoms/H2.jsx
+++ src/components/Atoms/H2.jsx
@@ -3,7 +3,7 @@ export default function H2({text}){
    <h2 className="
         text-[#ff52b2]
         text-6xl
-        drop-shadow-lg
+        drop-shadow-xl
         pt-[70px] pr-0 pb-0 pl-[10px]
         w-100% 
         max-w-[1000px]
