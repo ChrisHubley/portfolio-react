2f2188f05dafa521643f04c11a79aad9e06fd93f
Chris Hubley <chrisjhubley@gmail.com>
2025-02-10 17:55:33 +0000

    finished about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,8 +2,16 @@ import AboutText from "../components/AboutText";
 
 export default function About(){
     return(
-        <section>
-            <AboutText />
+        <section className="bg-[url(images/background2.jpg)]">
+            <div className="grid grid-cols-[2fr_1fr]
+            w-100% max-w-[1000px]
+            my-0 mx-auto ">
+                <AboutText />
+                <img className="
+                max-w-[300px] rounded-full mt-[30px] mr-[30px]  mb-[20px]
+                drop-shadow-[10px_5px_0_rgba(255,229,87,1)]"
+                src="./src/images/my_picture.jpg" alt="" />
+            </div>
         </section>
     )
 }
